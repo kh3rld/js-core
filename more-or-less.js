b72fe4e4b24e59e662aea9dcf5611453719cfd8d
khahussein <kherld.hussein@gmail.com>
2024-09-30 15:27:47 +0300

    Fix(function): Implement more-or-less js functions

--- more-or-less.js
+++ more-or-less.js
@@ -1,5 +1,5 @@
 const more = (n) => n + 1
 const less = (n) => n - 1
 const add = (n, n2) => n + n2
-const sub = function (n, n2) { return n2 - n }
+const sub = function (n, n2) { return n - n2 }
 console.log(sub(3, 10));
