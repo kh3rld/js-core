cb16cba9580a4aa1762c0919effc74a337c106d7
khahussein <kherld.hussein@gmail.com>
2024-09-30 11:47:53 +0300

    Feat(declarations): Implement Primitive and Operators objects

---
+++ declarations.js
@@ -0,0 +1,17 @@
+let escapeStr = `, \, /, "`
+let arr = [4, '2']
+let obj = {
+    str: 'value',
+    num: 1,
+    bool: true,
+    undef: undefined
+}
+let nested = {
+    arr: [4, undefined, '2'],
+    obj: {
+        str: 'value',
+        num: 1,
+        bool: true,
+        undef: undefined
+    }
+}
