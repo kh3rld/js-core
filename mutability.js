dd1af14410675531b9a3b18800432189f6a66e67
khahussein <kherld.hussein@gmail.com>
2024-09-30 15:08:29 +0300

    Fix(mutability): Implement 3 Ways to Copy Objects in JavaScript

--- mutability.js
+++ mutability.js
@@ -1,7 +1,10 @@
+// Using the JSON.stringify() and JSON.parse() methods
 let clone1 = JSON.parse(JSON.stringify(person));
 Object.freeze(clone1);
+// Using the spread (...) syntax
 let clone2 = { ...person };
 Object.freeze(clone2);
 person.age = 78
 person.country = "FR"
-let samePerson = Object.assign({}, person);
+// Using the Object.assign() method
+let samePerson = person
