c5f5a741f96a3116679ba6d7629ba35e00430628
khahussein <kherld.hussein@gmail.com>
2024-10-02 00:15:40 +0300

    docs(last-first-kiss): Add documentation to last-first-kiss Implementation

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,3 +1,15 @@
+/*
+Instructions
+
+Create 3 functions:
+
+    first: that takes an array or a string and returns its first element or character.
+
+    last: that takes an array or a string and return its last element or character.
+
+    kiss: that takes an array or string, and returns an array of 2 elements. The returned array should contain the last and first elements or characters, in that order.
+
+ */
 let arr = ["hello", "World", "I", "am", "Here"]
 function first(arr) {
     return arr[0]
