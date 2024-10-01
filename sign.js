b3486e3d3ffb539536b78c90b5bce1a93a4979f0
khahussein <kherld.hussein@gmail.com>
2024-10-01 23:46:29 +0300

    docs(sign): Add documentation to sign Implementation

--- sign.js
+++ sign.js
@@ -1,13 +1,21 @@
+/* 
+Create a function named sign that takes one number argument; returning 1 if the number is positive, -1 if the number is negative and 0 if the number is exactly 0.
+
+You must not just use Math.sign, make your own.
+
+Create a function named sameSign that takes 2 numbers as arguments and returns true if they both have the same sign, or false otherwise.
+*/
+
 function sign(n) {
     if (n == 0) {
         return 0
     }
     return (n > 0) ? 1 : -1
 }
-function sameSign(n,n2) {
+function sameSign(n, n2) {
     if (sign(n2) == sign(n)) {
         return true
-    } 
+    }
     return false
 }
 console.log(sign(10))
