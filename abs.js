c6d73526d6591daab9219974c844d8fc58a8f407
khahussein <kherld.hussein@gmail.com>
2024-10-01 23:54:17 +0300

    docs(abs): Add documentation to abs Implementation

--- abs.js
+++ abs.js
@@ -1,3 +1,10 @@
+/*
+Instructions
+
+Create a function named isPositive that takes a number as a argument, returning true if the number is strictly positive, and false otherwise.
+
+Create a function named abs that takes a number as an argument and returns its absolute value. You must make your own implementation. You must not use Math.abs().
+*/
 function isPositive(n) {
     return (n > 0) ? true : false
 }
