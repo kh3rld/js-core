619770e3cf64c85048af657bab60705764593d9f
khahussein <kherld.hussein@gmail.com>
2024-10-02 00:05:16 +0300

    docs(change): Add documentation to change Implementation

--- change.js
+++ change.js
@@ -7,6 +7,15 @@ Create 2 functions:
 
     set: a function that takes a key and a value. Update the value for the corresponding property of the sourceObject and return the value.
 
+    Code provided
+    The provided code will be added to your solution, and does not need to be submitted.
+    const sourceObject = {
+        num: 42,
+        bool: true,
+        str: 'some text',
+        log: console.log,
+    }
+
  */
 function get(keys) {
     return sourceObject[keys]
