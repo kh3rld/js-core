ff12fd6a1282d905f9f057df65a2c38c0331eb59
khahussein <kherld.hussein@gmail.com>
2024-10-01 23:57:53 +0300

    docs(biggie-smalls): Add documentation to biggie-smalls Implementation

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,2 +1,12 @@
+/*
+Instructions
+
+Create 2 variables:
+
+    smalls with the smallest possible number value.
+    biggie with the greatest possible number value.
+
+*/
+
 let smalls = Number.NEGATIVE_INFINITY;
 let biggie = Number.POSITIVE_INFINITY;
