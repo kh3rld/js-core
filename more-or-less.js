76f2ac215d8855997bd39850e93d56fcc597f860
khahussein <kherld.hussein@gmail.com>
2024-10-02 00:18:38 +0300

    docs(more-or-less): Add documentation to more-or-less Implementation

--- more-or-less.js
+++ more-or-less.js
@@ -1,3 +1,15 @@
+/*
+Instructions
+
+Create 4 functions:
+
+    more that takes 1 argument and adds 1 to it.
+    less that takes 1 argument and subtracts 1 from it.
+    add that takes 2 arguments and adds them together.
+    sub that takes 2 arguments and subtracts the second argument from the first.
+
+All the functions must return the result of the operations specified above.
+ */
 const more = (n) => n + 1
 const less = (n) => n - 1
 const add = (n, n2) => n + n2
