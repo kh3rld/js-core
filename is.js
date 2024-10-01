7e1ea790fe89b704c4aff6a11e4990362c7c4f9a
khahussein <kherld.hussein@gmail.com>
2024-10-01 23:48:07 +0300

    docs(is): Add documentation to is Implementation

--- is.js
+++ is.js
@@ -1,3 +1,29 @@
+/*
+Instructions
+
+Add new function properties to the is object to check value types. Each function should take one argument, and return a boolean.
+
+    is.num: value is a number.
+    is.nan: value is NaN.
+    is.str: value is a string.
+    is.bool: value is a boolean.
+    is.undef: value is undefined.
+    is.def: value is defined.
+    is.arr: value is an array.
+    is.obj: value is a simple object or null objects.
+    is.fun: value is a function.
+    is.truthy: value is truthy.
+    is.falsy: value is falsy.
+
+Examples
+
+console.log(is.num(5))
+// output: true
+
+console.log(is.num('ciao'))
+// output: false
+
+*/
 const is = {};
 
 is.num = function (n) {
