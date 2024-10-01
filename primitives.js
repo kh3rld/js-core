7ebc28e0106f53c7a22f145d8211e84f6d6c276b
khahussein <kherld.hussein@gmail.com>
2024-10-02 00:26:33 +0300

    docs(primitives): Add documentation to primitives Implementation

--- primitives.js
+++ primitives.js
@@ -1,3 +1,13 @@
+/*
+Instructions
+
+Create a constant variable for each of the following primitive types:
+
+    str for string
+    num for number
+    bool for boolean
+    undef for undefined
+ */
 const str = ""
 const num = 1
 const bool = true
