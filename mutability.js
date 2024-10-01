363d3ca7bc57131652a6211b337c8fd6c22af567
khahussein <kherld.hussein@gmail.com>
2024-10-02 00:23:03 +0300

    docs(mutability): Add documentation to mutability Implementation

--- mutability.js
+++ mutability.js
@@ -1,3 +1,20 @@
+/*
+Instructions
+
+Create three copies of the person object named clone1, clone2 and samePerson.
+
+Increase the age of person by one, and set its country to 'FR'.
+
+You must find a way to keep the original values of clone1 and clone2. The values of samePerson should change when person is changed.
+    Code provided
+    The provided code will be added to your solution, and does not need to be submitted.
+    const person = {
+        name: 'Rick',
+        age: 77,
+        country: 'US',
+    }
+*/
+
 // Using the JSON.stringify() and JSON.parse() methods
 let clone1 = JSON.parse(JSON.stringify(person));
 Object.freeze(clone1);
