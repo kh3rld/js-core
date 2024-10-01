8da5f33c772c9c1847948a872fca62d8d329e956
khahussein <kherld.hussein@gmail.com>
2024-10-01 17:18:00 +0300

    Feat(min-max): Implement custom maximum and and minimum numbers

---
+++ min-max.js
@@ -0,0 +1,14 @@
+function max(n,n2) {
+    if (n > n2) {
+        return n
+    }
+    return n2
+}
+function min(n,n2) {
+    if (n < n2) {
+        return n
+    }
+    return n2
+}
+console.log(max(3,4))
+console.log(min(1,2))
