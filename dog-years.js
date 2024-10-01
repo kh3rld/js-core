0f1bede19c656e59b323425bea49d90058f716c9
khahussein <kherld.hussein@gmail.com>
2024-10-01 12:50:51 +0300

    Feat(dogyrs): Implement dog-years calculation

--- dog-years.js
+++ dog-years.js
@@ -1,14 +1,15 @@
+const planet = {
+    earth: 1.0,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+}
+
 function dogYears(name, age) {
-    let planet = {
-        earth: 1.0,
-        mercury: 0.2408467,
-        venus: 0.61519726,
-        mars: 1.8808158,
-        jupiter: 11.862615,
-        saturn: 29.447498,
-        uranus: 84.016846,
-        neptune: 164.79132,
-    }
     ageSec = 31557600
     ageYrs = age / ageSec
     if (planet[name]) {
@@ -17,3 +18,4 @@ function dogYears(name, age) {
         return parseFloat(dogYrs.toFixed(2))
     }
 }
+console.log(dogYears("earth", 1000000000))
