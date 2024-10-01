c6be64cafcf654e45b45b13ebb19dbd05d34329c
khahussein <kherld.hussein@gmail.com>
2024-10-01 23:32:47 +0300

    Feat(typeof): implementation to correctly check for various value types as specified in instructions

--- is.js
+++ is.js
@@ -1,38 +1,37 @@
-const is = {
-    num(n) {
-        return typeof n === "number";
-    },
-    nan(n) {
-        return Number.isNaN(n);
-    },
-    str(s) {
-        return typeof s === "string";
-    },
-    bool(b) {
-        return typeof b === "boolean";
-    },
-    undef(u) {
-        return typeof u === "undefined";
-    },
-    def(d) {
-        return typeof d !== "undefined";
-    },
-    arr(a) {
-        return Array.isArray(a);
-    },
-    obj(o) {
-        return o !== null && typeof o === "object" && !Array.isArray(o) && !this.fun(o);
-    },
-    fun(f) {
-        return typeof f === "function";
-    },
-    truthy(value) {
-        return !!value;
-    },
-    falsy(value) {
-        return !value;
-    }
+function num(n) {
+    return typeof n === "number";
+}
+function nan(n) {
+    return Number.isNaN(n);
 };
+function str(s) {
+    return typeof s === "string";
+}
+function bool(b) {
+    return typeof b === "boolean";
+}
+function undef(u) {
+    return typeof u === "undefined";
+}
+function def(d) {
+    return typeof d !== "undefined";
+}
+function arr(a) {
+    return Array.isArray(a);
+}
+function obj(o) {
+    return o !== null && typeof o === "object" && !Array.isArray(o) && !this.fun(o);
+}
+function fun(f) {
+    return typeof f === "function";
+}
+function truthy(value) {
+    return !!value;
+}
+function falsy(value) {
+    return !value;
+}
+
 
 console.log(is.num(10));
 console.log(is.nan(NaN));
