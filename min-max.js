4bf62e1d09c7d1884eff538ec03a04760e9387a4
khahussein <kherld.hussein@gmail.com>
2024-10-01 23:52:42 +0300

    docs(min-max): Add documentation to min-max Implementation

--- min-max.js
+++ min-max.js
@@ -1,14 +1,26 @@
-function max(n,n2) {
+/*
+Instructions
+
+Create a function named max that takes 2 number arguments and returns the largest of the two.
+
+    You must not just use Math.max, make your own.
+
+Create a function named min which is the same as max, but returns the lowest.
+
+    You must not just use Math.min, make your own.
+
+*/
+function max(n, n2) {
     if (n > n2) {
         return n
     }
     return n2
 }
-function min(n,n2) {
+function min(n, n2) {
     if (n < n2) {
         return n
     }
     return n2
 }
-console.log(max(3,4))
-console.log(min(1,2))
+console.log(max(3, 4))
+console.log(min(1, 2))
