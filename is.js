6c8fb528234d58d5a4f39e55f52bb5ebbd89cc8a
khahussein <kherld.hussein@gmail.com>
2024-10-01 23:41:52 +0300

    Fix(typeof): Update implementation to correctly check for various value types as specified in instructions

--- is.js
+++ is.js
@@ -1,45 +1,57 @@
-function num(n) {
+const is = {};
+
+is.num = function (n) {
     return typeof n === "number";
-}
-function nan(n) {
+};
+
+is.nan = function (n) {
     return Number.isNaN(n);
 };
-function str(s) {
-    return typeof s === "string";
-}
-function bool(b) {
-    return typeof b === "boolean";
-}
-function undef(u) {
-    return typeof u === "undefined";
-}
-function def(d) {
-    return typeof d !== "undefined";
-}
-function arr(a) {
-    return Array.isArray(a);
-}
-function obj(o) {
-    return o !== null && typeof o === "object" && !Array.isArray(o) && !this.fun(o);
-}
-function fun(f) {
-    return typeof f === "function";
-}
-function truthy(value) {
-    return !!value;
-}
-function falsy(value) {
-    return !value;
-}
 
-// console.log(is.num(10));
-// console.log(is.nan(NaN));
-// console.log(is.str('ciao'));
-// console.log(is.bool(true));
-// console.log(is.undef(undefined));
-// console.log(is.def(null));
-// console.log(is.arr([1, 2, 3]));
-// console.log(is.obj({}));
-// console.log(is.fun(function () { }));
-// console.log(is.truthy(1));
-// console.log(is.falsy(0)); 
+is.str = function (s) {
+    return typeof s === "string";
+};
+
+is.bool = function (b) {
+    return typeof b === "boolean";
+};
+
+is.undef = function (u) {
+    return typeof u === "undefined";
+};
+
+is.def = function (d) {
+    return typeof d !== "undefined";
+};
+
+is.arr = function (a) {
+    return Array.isArray(a);
+};
+
+is.obj = function (o) {
+    return o !== null && typeof o === "object" && !Array.isArray(o);
+};
+
+is.fun = function (f) {
+    return typeof f === "function";
+};
+
+is.truthy = function (t) {
+    return !!t;
+};
+
+is.falsy = function (f) {
+    return !f;
+};
+
+console.log(is.num(10));
+console.log(is.nan(NaN));
+console.log(is.str('ciao'));
+console.log(is.bool(true));
+console.log(is.undef(undefined));
+console.log(is.def(null));
+console.log(is.arr([1, 2, 3]));
+console.log(is.obj({}));
+console.log(is.fun(function () { }));
+console.log(is.truthy(1));
+console.log(is.falsy(0)); 
