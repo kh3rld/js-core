bb488534bd4b5f86e9a7b8721a0b882492db7f17
khahussein <kherld.hussein@gmail.com>
2024-10-02 00:17:06 +0300

    docs(returns): Add documentation to returns Implementation

--- returns.js
+++ returns.js
@@ -1,6 +1,15 @@
+/*
+Instructions
+
+Create the following functions:
+
+    id that takes one argument and returns it.
+    getLength that takes an array or a string and returns its length.
+
+ */
 let arg = 9
 const id = (arg) => arg
 console.log(id(arg))
-function getLength (arr = ["string"]) { 
+function getLength(arr = ["string"]) {
     return arr.length
 }
