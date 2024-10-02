ac21cdf8e941be03889190366a9b2aa0b05fa6d2
khahussein <kherld.hussein@gmail.com>
2024-10-02 12:57:14 +0300

    docs(repeat): Add documentation

--- repeat.js
+++ repeat.js
@@ -1,3 +1,8 @@
+/*
+Instructions
+
+Create a function named repeat that takes a string and a number as arguments, and returns the string repeated as many times as the number describes. It should function like String.repeat(), but of course you must make your own.
+ */
 function repeat(s,n) {
     return Array(n).fill(s).join('')
 }
