6215ecdd689ffb83e15fd4cbde7f02df15c48215
kh3rld <kherld.hussein@gmail.com>
2024-10-05 01:19:33 +0300

    Fix(pyramid): Update pyramid implementation

--- pyramid.js
+++ pyramid.js
@@ -15,18 +15,17 @@ Output example
     No new line in last line
  */
 function pyramid(char, h) {
-  let result = "";
-  const charLength = char.length;
-
-  for (let i = 1; i <= h; i++) {
-    let chars = char.repeat(2 * i - 1);
-    let baseWidth = charLength * (2 * h - 1);
-    let levelWidth = charLength * (2 * i - 1);
-    let spaces = " ".repeat((baseWidth - levelWidth) / 2);
-    if (i !== h) {
-      result += spaces + chars + "\n";
+  let res = "";
+  for (let i = 0; i < h; i++) {
+    let spaces = " ".repeat((h - i - 1) * char.length);
+    let chars = char.repeat(i * 2 + 1);
+    if (i !== h - 1) {
+      res += spaces + chars + "\n";
     } else {
-      result += spaces + chars;
+      res += spaces + chars;
     }
   }
+  return res;
 }
+
+console.log(pyramid("{}", 12));
