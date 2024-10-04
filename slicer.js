d0dc3ac7f93cba4805253ccc639187cb5a9fff3b
khahussein <kherld.hussein@gmail.com>
2024-10-04 18:32:05 +0300

    Fix(slice): Implement slicer

--- slicer.js
+++ slicer.js
@@ -10,33 +10,40 @@ It takes 3 arguments:
     optional ending index.
 
  */
+Array.prototype.slice = undefined
+String.prototype.slice = undefined
 
 function slice(str, s, e) {
+
     if (s < 0) {
-        s = Math.max(0, str.length + s)
+        s = Math.max(0, str.length + s);
     } else {
-        s = Math.min(str.length, s)
+        s = Math.min(str.length, s);
     }
     if (e === undefined) {
-        e = str.length
+        e = str.length;
     } else if (e < 0) {
-        e = Math.max(0, (str.length + e))
+        e = Math.max(0, str.length + e);
     } else {
-        e = Math.min(str.length, s)
+        e = Math.min(str.length, e);
     }
     let r;
     if (typeof str === 'string') {
-        r = ""
+        r = '';
         for (let i = s; i < e; i++) {
             r += str[i];
         }
     } else if (Array.isArray(str)) {
-        r = []
+        r = [];
         for (let i = s; i < e; i++) {
-            r.push(str[i]);
+            r.push(arr[i]);
         }
     } else {
         throw new TypeError("Invalid Input")
     }
     return r
 }
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+const arr = "Kherld"
+console.log(slice(arr, 0, 2))
+console.log(slice(animals, 0, 2));
