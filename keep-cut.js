60d53c97ddf6c355bdff94a5ee4d0c0c776f5b35
kh3rld <kherld.hussein@gmail.com>
2024-10-04 22:46:56 +0300

    Fix(keep-cut): Update keepFirstLast implementation

--- keep-cut.js
+++ keep-cut.js
@@ -1,13 +1,13 @@
 /**
- * Instructions
+ * Insuctions
 
-Create some functions, which each take a string as an argument:
+Create some functions, which each take a sing as an argument:
 
-    cutFirst: returns the string with the first 2 characters removed.
+    cutFirst: returns the sing with the first 2 characters removed.
 
-    cutLast returns the string with the last 2 characters removed.
+    cutLast returns the sing with the last 2 characters removed.
 
-    cutFirstLast returns the string with the first 2 characters and the last 2 characters removed.
+    cutFirstLast returns the sing with the first 2 characters and the last 2 characters removed.
 
     keepFirst returns only the first 2 characters.
 
@@ -18,34 +18,30 @@ Create some functions, which each take a string as an argument:
  */
 
 function cutFirst(s) {
-    return s.slice(2);
+  return s.length > 2 ? s.slice(2) : "";
 }
 
 function cutLast(s) {
-    return s.slice(0, -2);
+  return s.length > 2 ? s.slice(0, -2) : "";
 }
-
 function cutFirstLast(s) {
-    return s.slice(2, -2);
+  return s.length >= 4 ? s.slice(2, -2) : "";
 }
-
 function keepFirst(s) {
-    return s.slice(0, 2);
+  return s.length >= 1 ? s.slice(0, 2) : "";
 }
-
 function keepLast(s) {
-    return s.slice(-2);
+  return s.length >= 1 ? s.slice(-2) : "";
 }
-
 function keepFirstLast(s) {
-    return s.slice(0, 2) + s.slice(-2);
+  return s.length <= 4 ? s : s.slice(0, 2) + s.slice(-2);
 }
 
 const str = "Hello, Kherld";
 
-console.log(cutFirst(str));     
-console.log(cutLast(str));       
-console.log(cutFirstLast(str));  
-console.log(keepFirst(str));     
-console.log(keepLast(str));      
-console.log(keepFirstLast(str)); 
+console.log(cutFirst(str));
+console.log(cutLast(str));
+console.log(cutFirstLast(str));
+console.log(keepFirst(str));
+console.log(keepLast(str));
+console.log(keepFirstLast(str));
