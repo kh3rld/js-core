c434909118756861df6a06708400e4813c9339c5
kh3rld <kherld.hussein@gmail.com>
2024-10-04 23:55:06 +0300

    Fix(unbreakable): Update split and join func

--- unbreakable.js
+++ unbreakable.js
@@ -1,25 +1,41 @@
 function split(str, sep) {
   let result = [];
-  let idx = 0;
+  let idx = "";
+
   if (sep === "") {
     for (let i = 0; i < str.length; i++) {
       result.push(str[i]);
     }
     return result;
   }
-  while (idx < str.length) {
-    const nextIdx = str.indexOf(sep, idx);
-    if (nextIdx === -1) {
-      result.push(str.slice(idx));
-      break;
+  for (let i = 0; i <= str.length; i++) {
+    if (i === str.length || str[i] === sep[0]) {
+      let match = true;
+      for (let j = 0; j < sep.length; j++) {
+        if (str[i + j] !== sep[j]) {
+          match = false;
+          break;
+        }
+      }
+      if (match) {
+        result.push(str.slice(idx, i));
+        idx = i + sep.length;
+        i += sep.length - 1;
+      }
     }
-    result.push(str.slice(idx, nextIdx));
-    idx = nextIdx + sep.length;
   }
-
+  result.push(str.slice(idx));
   return result;
 }
 
 function join(arr, sep) {
-  return arr.join(sep);
+  let result = "";
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+
+    if (i < arr.length - 1) {
+      result += sep;
+    }
+  }
+  return result;
 }
