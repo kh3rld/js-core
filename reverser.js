c052389c5eae16875ba15354840a320f864ddb41
kh3rld <kherld.hussein@gmail.com>
2024-10-04 23:18:25 +0300

    Fix(reverse): Implement Reverse

--- reverser.js
+++ reverser.js
@@ -1,12 +1,10 @@
- const rev = [];
 function reverse(arr) {
+  const rev = [];
   if (typeof arr === "string" && !Array.isArray(arr)) {
     for (let i = arr.length - 1; i >= 0; i--) {
       rev.push(arr[i]);
     }
-    return typeof arr === "string"
-      ? rev.join("")
-      : rev.join(",");
+    return typeof arr === "string" ? rev.join("") : rev.join(",");
   } else if (Array.isArray(arr)) {
     let result = [];
     for (let i = arr.length - 1; i >= 0; i--) {
