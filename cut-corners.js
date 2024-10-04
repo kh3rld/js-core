1f01de62871fa7ef28630fdfbc9b8bff4635f6e0
kh3rld <kherld.hussein@gmail.com>
2024-10-04 22:56:28 +0300

    Refactor(cut-conners): Remove tests

--- cut-corners.js
+++ cut-corners.js
@@ -14,9 +14,3 @@ function trunc(num) {
   return num < 0 ? Math.ceil(num) : Math.floor(num);
 }
 
-const nums = [3.7, -3.7, 3.1, -3.1];
-
-console.log(nums.map(round));
-console.log(nums.map(floor));
-console.log(nums.map(trunc));
-console.log(nums.map(ceil));
