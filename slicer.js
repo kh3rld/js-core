195c194ff9e4ed20282b1ca5e93f0b043f3579f6
khahussein <kherld.hussein@gmail.com>
2024-10-04 17:48:18 +0300

    Feat(slice): Implement slicer

---
+++ slicer.js
@@ -0,0 +1,31 @@
+function slice(str, s, e) {
+    if (s < 0) {
+        s = Math.max(0, (str.length + s))
+    } else {
+        s = Math.min(str.length, s)
+    }
+    if (e === undefined) {
+        e = str.length
+    } else if (e < 0) {
+        e = Math.max(0, (str.length + e))
+    } else if (e > 0) {
+        e = Math.min(str.length, s)
+    }
+    let r = "";
+    if (typeof str === "string") {
+        r = ""
+        for (let i = s; i < e; i++) {
+            r += str[i];
+        }
+    }
+    if (Array.isArray(str)) {
+        r = []
+        for (let i = s; i < e; i++) {
+            r.push(str[i]);
+        }
+    }
+    return r
+}
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+console.log(slice(animals, -2));
