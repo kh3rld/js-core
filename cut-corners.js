b6e3a507b102d38943ab652e2a79e17e4f06e6ba
kh3rld <kherld.hussein@gmail.com>
2024-10-04 23:02:03 +0300

    fix(Cut-Conners): Implement custom rounding functions
    - Added `round`: Mimics Math.round() behavior for rounding to the nearest integer.
    - Added `ceil`: Mimics Math.ceil() behavior for rounding up to the nearest integer.
    - Added `floor`: Mimics Math.floor() behavior for rounding down to the nearest integer.
    - Added `trunc`: Mimics Math.trunc() behavior for removing fractional digits.

--- cut-corners.js
+++ cut-corners.js
@@ -1,16 +1,49 @@
+function checkInt(num) {
+    const sign = num >= 0 ? 1 : -1;
+    num = Math.abs(num)
+
+    let placeValue = 1
+    let result = 0
+
+    while (placeValue <= num) {
+        placeValue *= 10
+    }
+    placeValue /= 10
+
+    while (placeValue >= 1) {
+        while (result + placeValue <= num) {
+            result += placeValue
+        }
+        placeValue /= 10
+    }
+    return sign * result
+}
+
 function round(num) {
-  return num < 0 ? Math.ceil(num - 0.5) : Math.floor(num + 0.5);
+   let newInt = checkInt(num)
+   let dec = Math.abs(num-newInt)
+   if (dec < 0.5) {
+    return newInt
+   }
+   return num >= 0 ? newInt+1 : newInt-1
 }
 
 function ceil(num) {
-  return num < 0 ? -Math.floor(-num) : Math.floor(num) + (num % 1 > 0 ? 1 : 0);
+    let newInt = checkInt(num)
+    if (num === newInt) {
+        return num
+    }
+    return num> 0? newInt+1 : newInt
 }
 
 function floor(num) {
-  return num < 0 ? -Math.ceil(-num) : Math.floor(num);
+    let newInt = checkInt(num)
+    if (num === newInt) {
+        return num
+    }
+    return num  > 0 ? newInt : newInt-1
 }
 
 function trunc(num) {
-  return num < 0 ? Math.ceil(num) : Math.floor(num);
+    return checkInt(num)
 }
-
