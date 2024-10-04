ab74e5a240ae363f7b8d96641933db9733dc687f
kh3rld <kherld.hussein@gmail.com>
2024-10-04 10:27:46 +0300

    docs(index): Update docs

--- index-of.js
+++ index-of.js
@@ -1,3 +1,14 @@
+/*
+Instructions
+
+Create 3 functions which accept an array to be searched, and a value to be matched.
+
+    indexOf: which returns the index of the first occurrence. It also accepts an optional index from where the search should begin. If the value was not found, -1 is returned.
+    lastIndexOf: which works just like your indexOf function, but returns the index of the last occurrence.
+    includes: which returns true if the value was found in the array, and false otherwise.
+
+    Of course you must not use any of Array.indexOf(), Array.lastIndexOf() or Array.includes().
+ */
 function indexOf(arr, val) {
     let idx = 0;
     if (idx < 0) {
