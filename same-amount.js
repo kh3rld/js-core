313d30c1050fb7b7d8e1006a12160f461e48eff9
kh3rld <kherld.hussein@gmail.com>
2024-10-05 23:28:28 +0300

    Fix: Same Amount, Create new regular expression

--- same-amount.js
+++ same-amount.js
@@ -1,16 +1,7 @@
 function sameAmount(str, reg1, reg2) {
-  const countMatches = (regex) => {
-    let count = 0;
-    let match;
-    regex.lastIndex = 0;
-    while ((match = regex.exec(str)) !== null) {
-      count++;
-    }
-    return count;
-  };
-
-  const count1 = countMatches(reg1);
-  const count2 = countMatches(reg2);
-
+  const r1 = new RegExp(reg1, "g");
+  const r2 = new RegExp(reg2, "g");
+  const count1 = (str.match(r1) || []).length;
+  const count2 = (str.match(r2) || []).length;
   return count1 === count2;
 }
