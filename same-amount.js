047650fc99a1bbe2feff32ea2720529c8e99b064
kh3rld <kherld.hussein@gmail.com>
2024-10-05 23:30:56 +0300

    docs(same-amount): Add documentation question

--- same-amount.js
+++ same-amount.js
@@ -1,3 +1,10 @@
+/*
+Instructions
+
+Create a function named sameAmount, that takes three arguments: a string, and 2 regular expressions. Your function should return a boolean.
+
+The objective is to confirm that the regular expressions match the string the same number of times.
+ */
 function sameAmount(str, reg1, reg2) {
   const r1 = new RegExp(reg1, "g");
   const r2 = new RegExp(reg2, "g");
