2f713d9d2fa5e35155da1a15996029556044b82a
kh3rld <kherld.hussein@gmail.com>
2024-10-07 20:34:39 +0300

    Fix: Update first day week date implementation

--- get-some-time.js
+++ get-some-time.js
@@ -2,9 +2,14 @@ function firstDayWeek(week, year) {
   if (week < 1 || week > 53) {
     throw new Error("Week must be between 1 and 53.");
   }
+
   const startOfYear = new Date(year, 0, 1);
   const dayOfWeek = startOfYear.getDay();
-  const offset = dayOfWeek === 0 ? 1 : (1 - dayOfWeek + 7) % 7;
+  let offset = 1 - dayOfWeek;
+
+  if (dayOfWeek === 0) {
+    offset = 1;
+  }
 
   const firstMonday = new Date(startOfYear);
   firstMonday.setDate(startOfYear.getDate() + offset);
@@ -18,8 +23,3 @@ function firstDayWeek(week, year) {
 
   return formattedDate;
 }
-
-console.log(firstDayWeek(1, "1000"));
-console.log(firstDayWeek(1, "2024"));
-console.log(firstDayWeek(2, "2024"));
-console.log(firstDayWeek(53, "2024"));
