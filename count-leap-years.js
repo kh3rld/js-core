cbebb3bf68b9fec65e24c9aee0c4fb2c4beedae7
kh3rld <kherld.hussein@gmail.com>
2024-10-07 22:54:51 +0300

    Fix(count-leap-year): Iterate over the year to count leap years from Year 1 to Year N:

--- count-leap-years.js
+++ count-leap-years.js
@@ -5,11 +5,15 @@ Create a function named countLeapYears which accepts a Date. Your function shoul
  */
 function countLeapYears(date) {
   const year = date.getFullYear();
-  let leapYears = 0;
-  for (let i = 1; i <= year; i++) {
-    if ((i % 4 === 0 && i % 100 !== 0) || i % 400 === 0) {
-      leapYears++;
+  let count = 0;
+  for (let i = 1; i < year; i++) {
+    if (isLeapYear(i)) {
+      count++;
     }
   }
-  return leapYears;
+  return count;
+}
+
+function isLeapYear(year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
