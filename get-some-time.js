55d728db2d709e916ed77cb06fdf02d25979d7e0
kh3rld <kherld.hussein@gmail.com>
2024-10-07 20:17:24 +0300

    Fix: Update first day week date implementation

--- get-some-time.js
+++ get-some-time.js
@@ -6,16 +6,21 @@ function firstDayWeek(week, year) {
   const firstDayOfYear = new Date(year, 0, 1);
 
   const firstMonday = new Date(firstDayOfYear);
-  const dayOfWeek = firstMonday.getDay();
-  const daysUntilMonday = dayOfWeek === 0 ? 1 : 8 - dayOfWeek;
+  const dayOfWeek = firstMonday.getUTCDay();
 
-  firstMonday.setDate(firstMonday.getDate() + daysUntilMonday);
+  const daysUntilMonday = dayOfWeek === 0 ? 1 : 8 - dayOfWeek;
+  firstMonday.setUTCDate(firstMonday.getUTCDate() + daysUntilMonday);
 
   const firstDayOfSpecifiedWeek = new Date(firstMonday);
-  firstDayOfSpecifiedWeek.setDate(firstMonday.getDate() + (week - 1) * 7);
-  const day = String(firstDayOfSpecifiedWeek.getDate()).padStart(2, "0");
-  const month = String(firstDayOfSpecifiedWeek.getMonth() + 1).padStart(2, "0");
+  firstDayOfSpecifiedWeek.setUTCDate(firstMonday.getUTCDate() + (week - 1) * 7);
+
+  const day = String(firstDayOfSpecifiedWeek.getUTCDate()).padStart(2, "0");
+  const month = String(firstDayOfSpecifiedWeek.getUTCMonth() + 1).padStart(
+    2,
+    "0"
+  );
   const formattedDate = `${day}-${month}-${year}`;
 
   return formattedDate;
 }
+
