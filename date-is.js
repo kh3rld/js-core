4e04f5fd60ae10036121fad5ec1ab1efe8e7ef8d
kh3rld <kherld.hussein@gmail.com>
2024-10-07 18:35:46 +0300

    Fix: Update isValid date implementation

--- date-is.js
+++ date-is.js
@@ -1,19 +1,34 @@
 function isValid(date) {
-  return date instanceof Date && !isNaN(date);
+  return (
+    (date instanceof Date && !isNaN(date)) ||
+    (typeof date === "number" && !isNaN(new Date(date)))
+  );
 }
 
 function isAfter(date1, date2) {
-  return isValid(date1) && isValid(date2) && date1 > date2;
+  if (!isValid(date1) || !isValid(date2)) {
+    throw new Error("Invalid date provided.");
+  }
+  return date1 > date2;
 }
 
 function isBefore(date1, date2) {
-  return isValid(date1) && isValid(date2) && date1 < date2;
+  if (!isValid(date1) || !isValid(date2)) {
+    throw new Error("Invalid date provided.");
+  }
+  return date1 < date2;
 }
 
 function isFuture(date) {
-  return isValid(date) && date > new Date();
+  if (!isValid(date)) {
+    throw new Error("Invalid date provided.");
+  }
+  return date > new Date();
 }
 
 function isPast(date) {
-  return isValid(date) && date < new Date();
+  if (!isValid(date)) {
+    throw new Error("Invalid date provided.");
+  }
+  return date < new Date();
 }
