d2bfefb141da1eed20bc4d6cfbf289d5b1cd4bf3
kh3rld <kherld.hussein@gmail.com>
2024-10-07 22:21:10 +0300

    docs(match-cron): Add question documentation

--- match-cron.js
+++ match-cron.js
@@ -1,3 +1,23 @@
+/*
+Instructions
+
+Create a function named matchCron, which accepts a valid cron string, and a valid Date. Your function should return true if the pattern matches the Date.
+
+    You only need to implement numbers and *. Other complex patterns are not required.
+
+Only valid patterns will be tested.
+Example
+
+matchCron('9 * * * *', new Date('2020-05-30 18:09:00')) // -> true
+matchCron('9 * * * *', new Date('2020-05-30 19:09:00')) // -> true
+matchCron('9 * * * *', new Date('2020-05-30 19:21:00')) // -> false
+//         | | | | |
+//         | | | | +- Day of the Week   (range: 1-7, 1 is Monday)
+//         | | | +--- Month of the Year (range: 1-12)
+//         | | +----- Day of the Month  (range: 1-31)
+//         | +------- Hour              (range: 0-23)
+//         +--------- Minute            (range: 0-59)
+ */
 function matchCron(cronString, date) {
   let cronParts = Array.isArray(cronString)
     ? cronString
