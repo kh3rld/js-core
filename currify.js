6ca1808a5a534c349599850cc0a0d359ba82861f
kh3rld <kherld.hussein@gmail.com>
2024-10-10 13:51:09 +0300

    Feat(currify): Implement currify function to enable currying of any function

--- currify.js
+++ currify.js
@@ -14,3 +14,20 @@ console.log(mult2Curried(2)(2)) // result expected 4
 // (same result, with a function that has technically only one argument)
  */
 
+function currify(fn) {
+  return function curried(...args) {
+    // If the number of arguments is enough to call the function, do it
+    if (args.length >= fn.length) {
+      return fn(...args);
+    }
+    // Otherwise, return a function that collects more arguments
+    return function (...nextArgs) {
+      return curried(...args, ...nextArgs);
+    };
+  };
+}
+
+const mult2 = (el1, el2) => el1 * el2;
+const mult2Curried = currify(mult2);
+console.log(mult2(2, 2));
+console.log(mult2Curried(2)(2));
