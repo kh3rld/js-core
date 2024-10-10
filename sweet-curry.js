d067c9a63fd33e33913bf944185a1098c02a2ebe
kh3rld <kherld.hussein@gmail.com>
2024-10-10 13:13:38 +0300

    Feat(sweet-curry): Implement functions with currying process

--- sweet-curry.js
+++ sweet-curry.js
@@ -7,3 +7,16 @@ Create the following functions with the "currying" process. Those functions shou
     add3: that adds three numbers.
     sub4: that subtracts four numbers in the given order: first number minus second number etc.
  */
+
+// Multiplies two numbers
+const mult2 = (a) => (b) => a * b;
+
+// Adds three numbers
+const add3 = (a) => (b) => (c) => a + b + c;
+
+// Subtracts four numbers in order
+const sub4 = (a) => (b) => (c) => (d) => a - b - c - d;
+
+console.log(mult2(3)(4));
+console.log(add3(1)(2)(3));
+console.log(sub4(10)(2)(3)(1));
