d7c756fcfff50e1d42139eead2693306d6086c74
kh3rld <kherld.hussein@gmail.com>
2024-10-10 12:11:00 +0300

    Doc(sweet-curry): Add question documentation

---
+++ sweet-curry.js
@@ -0,0 +1,9 @@
+/**
+ * Instructions
+
+Create the following functions with the "currying" process. Those functions should accept only one argument each.
+
+    mult2: that multiplies two numbers.
+    add3: that adds three numbers.
+    sub4: that subtracts four numbers in the given order: first number minus second number etc.
+ */
