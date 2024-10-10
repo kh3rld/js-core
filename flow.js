5434681288ab8e3f84bb1f93222edc59fb563b12
kh3rld <kherld.hussein@gmail.com>
2024-10-10 13:25:59 +0300

    Feat(flow): Implement flow function for function composition

--- flow.js
+++ flow.js
@@ -10,3 +10,11 @@ const add2Numbers = (nbr1, nbr2) => nbr1 + nbr2
 const flowedFunctions = flow([add2Numbers, square])
 flowedFunctions(2, 3) // -> 25
  */
+
+function flow(funcs) {
+  return function (...args) {
+    return funcs.reduce((acc, func) => {
+      return [func(...acc)];
+    }, args)[0];
+  };
+}
