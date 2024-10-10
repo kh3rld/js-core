238cc38b03c2753da407cdfcb095762eb471f6c3
kh3rld <kherld.hussein@gmail.com>
2024-10-10 11:41:38 +0300

    Doc(long-words): Add question documentation

--- long-words.js
+++ long-words.js
@@ -1,3 +1,15 @@
+/*
+Instructions
+
+Create three functions, which each accept an array as an argument.
+
+    longWords: returns true if every element of the array is a string with at least 5 characters.
+
+    oneLongWord: returns true if at least one element of the array is a string with 10 or more characters.
+
+    noLongWords: returns true if there are no elements in the array which is a string with at least 7 characters.
+ */
+
 // Long Words
 const longWords = (arr) => {
   return arr.every((word) => typeof word === "string" && word.length >= 5);
