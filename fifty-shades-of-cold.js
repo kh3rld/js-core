c704c4ff7412e63a910650654b9c2631835f6865
kh3rld <kherld.hussein@gmail.com>
2024-10-13 08:51:48 +0300

    feat(fifty-shades-of-cold): Generate cold color shades and update background on click
    - Created `generateClasses` function to generate CSS classes for each color in the `colors` array, applying corresponding background colors.
    - Implemented `generateColdShades` function to create `<div>` elements for shades of cold colors (aqua, blue, turquoise, green, cyan, navy, purple) and display their names.
    - Added `choseShade` function to update the background of all divs to the class of the clicked div.

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -28,3 +28,33 @@ You only need to create & submit the JS file fifty-shades-of-cold.js, we're prov
     feel free to use the CSS file fifty-shades-of-cold.data.css as it is or you can also modify it.
 
  */
+
+import { colors } from "./fifty-shades-of-cold.data.js";
+
+export function generateClasses() {
+  const head = document.getElementsByTagName("head")[0];
+  const style = document.createElement("style");
+  colors.forEach((color) => {
+    style.innerHTML += `.${color} { background: ${color} }`;
+  });
+  console.log(style.innerHTML);
+  head.appendChild(style);
+}
+
+export function generateColdShades() {
+  const body = document.getElementsByTagName("body")[0];
+  colors.forEach((color) => {
+    if (color.match(/(aqua|blue|turquoise|green|cyan|navy|purple)/) !== null) {
+      const div = document.createElement("div");
+      div.classList.add(color);
+      div.innerHTML = color;
+      body.appendChild(div);
+    }
+  });
+}
+
+export function choseShade(shade) {
+  document.querySelectorAll("div").forEach((div) => {
+    div.className = shade;
+  });
+}
