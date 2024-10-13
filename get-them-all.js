dea2f54b98289c21f3a13bfc3cae6600f8a2df82
kh3rld <kherld.hussein@gmail.com>
2024-10-13 08:26:25 +0300

    Feat(get-them-all): Implement function task to find the main architect of the Tower of Pisa before he achieves his plans

--- get-them-all.js
+++ get-them-all.js
@@ -49,5 +49,26 @@ You only need to create & submit the JS file get-them-all.js ; we're providing y
 
     feel free to use the CSS file get-them-all.data.css as it is or you can also modify it.
  */
+//getArchitects: find all  a tags and check those with class architects and non architects
+export const getArchitects = () => [
+  document.querySelectorAll("body a"),
+  document.querySelectorAll("body span"),
+];
 
+//getClassical : filters the architects further to find those in the classical class and those who arent
+export const getClassical = () => [
+  document.querySelectorAll("a.classical"),
+  document.querySelectorAll("a:not(.classical)"),
+];
 
+//getActive functionreturns 2 an array of  2 arrays one containig acitve and non active
+export const getActive = () => [
+  document.querySelectorAll("a.classical.active"),
+  document.querySelectorAll("a.classical:not(.active)"),
+];
+
+//getBonannoPisano identifies specific  archiect by their id and retrns all the remaining active classbcal architects
+export const getBonannoPisano = () => [
+  document.querySelector("#BonannoPisano"),
+  document.querySelectorAll("a.classical.active"),
+];
