8f67a5a8dd77b182906a99abc6acca473fa23896
kh3rld <kherld.hussein@gmail.com>
2024-10-14 11:46:50 +0300

    Feat(invert): Add invert function to swap keys and values of an object

---
+++ invert.js
@@ -0,0 +1,14 @@
+function invert(obj) {
+  const inverted = {};
+  for (const [key, value] of Object.entries(obj)) {
+    if (inverted.hasOwnProperty(value)) {
+      throw new Error(`Duplicate value found: ${value} for key ${key}.`);
+    }
+    inverted[value] = key;
+  }
+  return inverted;
+}
+
+const original = { "Kherld": 1, "Nerd": 2, "Me": 3 };
+const inverted = invert(original);
+console.log(inverted);
