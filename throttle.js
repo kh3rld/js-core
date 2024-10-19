79aaef09eadabab2c7b872d25e4f4fd11be2b323
kh3rld <kherld.hussein@gmail.com>
2024-10-19 07:07:00 +0300

    docs(throttle): Add documentation

---
+++ throttle.js
@@ -0,0 +1,8 @@
+/*
+ * Instructions
+
+Create two functions that will work like _.throttle from lodash.
+
+    throttle: don't worry about the options.
+    opThrottle: implement the trailing and leading options.
+ */
