4e71bb05f972a36f4a2649276239e48647a152ea
kh3rld <kherld.hussein@gmail.com>
2024-10-19 07:07:53 +0300

    Feat(throttle): Add throttle function
    - Implemented a throttle function to limit the execution of a given function to once every specified delay period.
    - Ensures that the function runs at most once in the defined interval, improving performance in high-frequency event scenarios.

--- throttle.js
+++ throttle.js
@@ -6,3 +6,14 @@ Create two functions that will work like _.throttle from lodash.
     throttle: don't worry about the options.
     opThrottle: implement the trailing and leading options.
  */
+
+function throttle(func, delay) {
+  let last = 0;
+  return function () {
+    const now = +new Date();
+    if (now - last > delay) {
+      func.apply(this, arguments);
+      last = now;
+    }
+  };
+}
