bb29c93f6a8e04f11e557c992d19cf8bdcd35182
kh3rld <kherld.hussein@gmail.com>
2024-10-19 07:10:02 +0300

    Feat(OPThrottle): Implement opThrottle function with configurable options
    - Added an opThrottle function that limits function execution to once per specified delay.
    - Supports leading and trailing invocation options for more flexible usage.
    - If leading is true, the function can execute immediately on the first call; if trailing is true, it will execute after the delay if not invoked again.

--- throttle.js
+++ throttle.js
@@ -17,3 +17,28 @@ function throttle(func, delay) {
     }
   };
 }
+
+function opThrottle(fn, delay, { leading = false, trailing = true } = {}) {
+  let last = 0;
+  let timer = null;
+  return function () {
+    const now = +new Date();
+    if (!last && leading === false) {
+      last = now;
+    }
+    if (now - last > delay) {
+      if (timer) {
+        clearTimeout(timer);
+        timer = null;
+      }
+      fn.apply(this, arguments);
+      last = now;
+    } else if (!timer && trailing !== false) {
+      timer = setTimeout(() => {
+        fn.apply(this, arguments);
+        last = +new Date();
+        timer = null;
+      }, delay);
+    }
+  };
+}
