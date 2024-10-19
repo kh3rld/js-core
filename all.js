da2526c216d5ff9e8b52506abc1ba355b1696cf3
kh3rld <kherld.hussein@gmail.com>
2024-10-19 08:14:53 +0300

    docs(all): Add question documentation

---
+++ all.js
@@ -0,0 +1,10 @@
+/*
+ * Instructions
+
+Create a function named all that works like Promise.all but with objects (instead of arrays).
+Code provided
+
+    The provided code will be added to your solution, and does not need to be submitted.
+
+Promise.all = undefined
+*/
