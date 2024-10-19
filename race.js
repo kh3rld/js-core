6dfe4ceb894d85df002501be41b65faf25a313d1
kh3rld <kherld.hussein@gmail.com>
2024-10-19 08:57:55 +0300

    Fix some function implementation to return first resolved values
    - Updated some function to handle input validation for empty arrays and zero count.
    - Utilized a promise to track resolution of individual promises and count the resolved results.
    - Ensured that the function resolves with the first `count` resolved values, filtering out any falsy results.
    - Improved error handling to reject the promise when count of resolved values is reached due to rejections.

--- race.js
+++ race.js
@@ -15,3 +15,26 @@ async function race(args) {
   });
   return newProm;
 }
+
+async function some(args, count) {
+  if (!args.length || !count) return Promise.resolve([]);
+  let newArr = new Array(args.length);
+  await new Promise((res, rej) => {
+    args.forEach((promise, i) => {
+      promise.then(
+        (x) => {
+          newArr[i] = x;
+          count--;
+          if (count <= 0) return res();
+        },
+        (y) => {
+          newArr[i] = y;
+          count--;
+          if (count <= 0) return rej();
+        },
+      );
+    });
+  });
+  newArr = newArr.filter((x) => x !== false);
+  return newArr;
+}
