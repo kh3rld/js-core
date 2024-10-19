121b26e6a602fd297ed2ff64e7b723a2b4462823
kh3rld <kherld.hussein@gmail.com>
2024-10-19 08:41:31 +0300

    docs(race): Add question documentation

---
+++ race.js
@@ -0,0 +1,8 @@
+/*
+ * Instructions
+
+Create two functions:
+
+    race: that works like Promise.race.
+    some: that takes an array of promises or values, and count number. It should return the first count resolved values. Empty arrays or a count of 0 return a promise resolving to undefined.
+*/
