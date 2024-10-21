df167ece3be7cf6c8e8a2f2da472a656b3da58e2
kh3rld <kherld.hussein@gmail.com>
2024-10-21 13:06:02 +0300

    docs(verydisco-reverse): Add question documentation

---
+++ verydisco-reverso.mjs
@@ -0,0 +1,13 @@
+/*Instructions
+
+Create a verydisco-reverso.mjs script that:
+
+    takes the name of a file (with the extension) as a first argument
+    reads this file
+    deciphers the content of this file by reversing it from the very disco mode
+    prints the result in the console
+
+For example:
+
+    Reading the verydisco content of your verydisco.txt file would print discovery in console.
+*/
