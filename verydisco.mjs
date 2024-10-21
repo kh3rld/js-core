3f7289918c4fb986008d138619d70768c05b7a8f
kh3rld <kherld.hussein@gmail.com>
2024-10-21 12:45:00 +0300

    docs(verydisco): Add question documentation

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,3 +1,32 @@
+/*
+ * Instructions
+
+Create a verydisco.mjs script that :
+
+    takes the 1st argument from the command line (after the program name)
+    makes it very disco:
+        cut each word from this argument in 2 (rounded up),
+        re-compose a word by concatenating the chunks in the other order
+    display the result in console
+
+If the argument passed is a sentence, each word of the sentence must be "very disco".
+
+For example:
+
+    discovery would print verydisco (🕺🏼) in console.
+    Node is awesome would print deNo si omeawes in the console.
+
+Notions
+
+    Node process: argv
+    nan-academy.github.io/js-training/examples/methods.js
+    slice() method
+    includes() method
+    split() method
+    join() method
+    Math.ceil() function
+*/
+
 // Function to make a word "very disco"
 function veryDisco(word) {
   const mid = Math.ceil(word.length / 2);
