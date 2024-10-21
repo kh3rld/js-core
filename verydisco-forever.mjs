bb05680fb167a976e14ad647d0e452fb3519cf6b
kh3rld <kherld.hussein@gmail.com>
2024-10-21 12:55:41 +0300

    docs(verydisco-forever): Add question documentation

---
+++ verydisco-forever.mjs
@@ -0,0 +1,5 @@
+/*
+ * Instructions
+
+Create a verydisco-forever.mjs script that does exactly the same as you verydisco script, but writes the result in a verydisco-forever.txt file instead of printing it in the console.
+*/
